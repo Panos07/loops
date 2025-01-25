080637f1b9b9b1c822dc84a11e047a15d08ec25a
Panos Nassos <164929181+Panos07@users.noreply.github.com>
2025-01-25 09:48:59 +0200

    Add files via upload

---
+++ askhsh 5.py
@@ -0,0 +1,10 @@
+import os
+os.system("cls")
+
+sum=0
+for counter in range(10):
+    yournumber=int(input("Δώσε έναν αριθμό "))
+    sum=sum+yournumber
+    
+mo=sum/10
+print(mo)
---
+++ ασκηση 10.py
@@ -0,0 +1,12 @@
+import os 
+os.system("cls")
+
+
+for i in range(10):
+    aritmos=float(input("Δώσε μου έναν αριθμό "))
+    
+    if (aritmos%2==0):
+        print("είναι ζυγός")
+    else:
+        print("είναι περιττός")
+    
